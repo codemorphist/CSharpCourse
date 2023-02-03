be5f753f62e45742f2fdfb83e857696fc1a9c28a
acidfade <alexkatrenkos@gmail.com>
2023-02-03 21:29:29 +0200

    Add HomeWork5.1

---
+++ HomeWork5.1/Geometry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._1
+{
+    internal class Geometry
+    {
+        private char[,] Figure;
+        private char symbol;
+        private char space;
+        
+        public Geometry(char symbol, char space) 
+        {
+
+        }
+    }
+}
---
+++ HomeWork5.1/HomeWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5._1
+{
+    internal class HomeWork
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
