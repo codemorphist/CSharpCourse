687ed2e06cc2c478fad99748bc5b9b8c681fae30
acidfade <alexkatrenkos@gmail.com>
2023-03-26 20:21:45 +0200

    Added HomeWork7

---
+++ HomeWork7/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace HomeWork7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.UserInfoLable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DatePicker
+            // 
+            this.DatePicker.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DatePicker.Location = new System.Drawing.Point(6, 28);
+            this.DatePicker.Name = "DatePicker";
+            this.DatePicker.Size = new System.Drawing.Size(245, 22);
+            this.DatePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Виберіть вашу дату народження:";
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(6, 56);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(245, 30);
+            this.CalculateButton.TabIndex = 2;
+            this.CalculateButton.Text = "Розрахувати";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CalculateButton_MouseClick);
+            // 
+            // UserInfoLable
+            // 
+            this.UserInfoLable.AutoSize = true;
+            this.UserInfoLable.Location = new System.Drawing.Point(3, 105);
+            this.UserInfoLable.Name = "UserInfoLable";
+            this.UserInfoLable.Size = new System.Drawing.Size(0, 16);
+            this.UserInfoLable.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 233);
+            this.Controls.Add(this.UserInfoLable);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DatePicker);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker DatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label UserInfoLable;
+    }
+}
+
---
+++ HomeWork7/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HomeWork7
+{
+    public partial class Form1 : Form
+    {
+        private DateTime UserBornDay;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            UserBornDay = new DateTime();
+        }
+
+        private int Years(DateTime start, DateTime end)
+        {
+            return (end.Year - start.Year - 1) +
+                   (((end.Month > start.Month) ||
+                   ((end.Month == start.Month) && (end.Day >= start.Day))) ? 1 : 0);
+        }
+
+        private int Days(DateTime start, DateTime end)
+        {
+            return Convert.ToInt32((end - start).TotalDays);
+        }
+
+        private int Month(DateTime start, DateTime end)
+        {
+            return Convert.ToInt32(((end.Year - start.Year) * 12) + end.Month - start.Month);
+        }
+
+        private int Eighteen(DateTime start)
+        {
+            return start.Year + 18;
+        }
+
+        private void CalculateButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            UserBornDay = DatePicker.Value.Date;
+
+            if (UserBornDay >= DateTime.Now)
+            {
+                UserInfoLable.Text = "Нажаль, ви ще не народилися!";
+                return;
+            }
+            
+            int years = Years(UserBornDay, DateTime.Now);
+            int days = Days(UserBornDay, DateTime.Now);
+            int month = Month(UserBornDay, DateTime.Now);
+            int eighteen = Eighteen(UserBornDay);
+
+            UserInfoLable.Text = $"Повних років: {years}\n" +
+                                 $"Ви прожили {days} днів\n" +
+                                 $"Ви прожили {month} місяців\n" +
+                                 ((years >= 18) ? $"Вам виповнилося 18 в {eighteen} році" : $"Вам виповнеться 18 в {eighteen}");
+        }
+    }
+}
